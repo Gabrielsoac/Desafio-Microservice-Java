7f8c426fb69e32ca88cfb47ca4da348aa6142015
Gabriel Soares <gabrielsoacc@gmail.com>
2024-12-16 20:34:40 -0300

    initial configs with spring boot

--- orders/src/main/java/com/gabrielsoac/orders/OrdersApplication.java
+++ orders/src/main/java/com/gabrielsoac/orders/OrdersApplication.java
@@ -1,13 +1,13 @@
-package com.challenge.backend;
+package com.gabrielsoac.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class OrdersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(OrdersApplication.class, args);
 	}
 
 }
--- orders/src/test/java/com/gabrielsoac/orders/OrdersApplicationTests.java
+++ orders/src/test/java/com/gabrielsoac/orders/OrdersApplicationTests.java
@@ -1,10 +1,10 @@
-package com.challenge.backend;
+package com.gabrielsoac.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class OrdersApplicationTests {
 
 	@Test
 	void contextLoads() {
